3fd0c1016557845b18e70c2163419da8b130ae8b
leonardo cuquejo soares <leonardo.s@cuquejo.org>
2020-03-13 14:58:08 +0000

    Ep 31 completed

--- front-end-app/src/App.js
+++ front-end-app/src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, I'm a React App</h1>
-    //   </div>
-    // );
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, '????'));
+    return (
+      <div className="App"> 
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really worki!</p>
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, '????'));
   }
 }
 
